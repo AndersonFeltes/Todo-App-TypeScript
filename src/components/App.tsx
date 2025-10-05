3cde5e02bf5e0cabb1e169525ac1f9574a44c5bf
Anderson Feltes <andermateus8@gmail.com>
2025-10-05 18:08:38 -0300

    modified App.tsx removed code comment

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,31 +1,3 @@
-// import TodoContext from "../contexts/TodoContext";
-// import { Navbar } from "./Navbar";
-// import { TodoList } from "./TodoList";
-// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-// import AddTodo from "./AddTodo";
-
-// export const App = () => {
-//   return (
-//     <TodoContext>
-//       <Router>
-//         <Navbar></Navbar>
-//         <br />
-//         <div className="uk-container">
-//           <Switch>
-//             <Route path="/create">
-//               <AddTodo></AddTodo>
-//             </Route>
-//             <Route path="/" exact>
-//               <h4>Minha lista de tarefas</h4>
-//               <TodoList></TodoList>
-//             </Route>
-//           </Switch>
-//         </div>
-//       </Router>
-//     </TodoContext>
-//   );
-// };
-
 import TodoProvider from "../contexts/TodoContext"; // ✅ default export
 import { Navbar } from "./Navbar";
 import { TodoList } from "./TodoList";
